ac1e7ee9c55e5fbf4b7abc6e5941891cd0adb8f3
Radif <i@radif.ru>
2021-05-01 20:52:53 +0300

    lesson07

--- js/vue_main.js
+++ js/vue_main.js
@@ -1,13 +1,45 @@
-const app = new Vue({
+Vue.component('goods-list', {
+    template: `<div class="parent-product" v-for="(product, id) in goods"
+                     :key="\`product_\${id}\`
+                     :goods="goods">
+                    <a class="product" href="shopping-cart.html">
+                        <img :src="product.img_src" :alt="product.img_alt">
+                        <p class="description-product">
+                            {{ product.product_name }}</p>
+                        <p class="price">\${{ product.price }}.00</p>
+                    </a>
+                    <div class="product-link-flex">
+                        <a class="add-to-cart buyBtn" href="#">
+                            <img src="img/cart-white.svg" alt="cart-white">
+                            Add to Cart
+                        </a>
+                    </div>
+                </div>`,
+    props: ['goods']
+    // data () {
+    //     return {
+    //         goods: "goods"
+    //     }
+    // }
+    // props: {
+    //     goods: {
+    //         type: Array,
+    //         required: false,
+    //         default: () => [] // () => ({})
+    //     }
+    // }
+});
+
+const vm = new Vue({
     el: '#root',
     data: {
         products_href: 'https://raw.githubusercontent.com/radif-ru/JavaScriptProfessional-v2/master/responses/catalogData.json',
         goods: [],
-        filter_goods: []
+        filtered_goods: []
     },
     computed: {},
     methods: {
-        getData(products_href) {
+        getData(products_href, err_counter = 5) {
             fetch(
                 products_href,
                 {
@@ -18,20 +50,31 @@ const app = new Vue({
             ).then(res => res.json()
             ).then(res => {
                 this.goods = res;
-                // this.filter_goods = res;
+                this.filtered_goods = res;
+            }).catch(error => {
+                if (err_counter > 1) {
+                    setTimeout(
+                        () => {
+                            console.log('catch error!', error);
+                            this.getData(products_href, err_counter -= 1);
+                        },
+                        3000
+                    );
+                }
             });
+            return this.goods
         },
-        filterGoods(value) {
+        filteredGoods(value) {
             const regexp = new RegExp(value, 'gmi');
-            this.filter_goods = this.goods.filter(product => regexp.test(product.name));
-            console.log(`filter_goods ${this.filter_goods}`);
+            this.filtered_goods = this.getData(this.products_href, 5).filter(product => regexp.test(product.name));
+            console.log(`filtered_goods ${this.filtered_goods}`);
             console.log(`goods ${this.goods}`);
         }
     },
     mounted() {
-        this.getData(this.products_href);
-        this.filterGoods('MANGO');
+        this.getData(this.products_href, 5);
+        this.filteredGoods('MANGO');
     }
 });
 
-console.log(app);
+console.log(vm);
